aea9be1629eaa7fddee739cca7161cd432dbeb80
Fauziya Abdul Rahman <frahman_128@yahoo.com>
2024-09-16 11:35:10 +0000

    first commit

---
+++ script.py
@@ -0,0 +1,9 @@
+name= input("John")
+print(f"Hello! What is your {name}?")
+age_input = input ("How old are you?")
+age = int(age_input)
+bot_age = 3
+age_difference = age - bot_age
+print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+color_input = input ("What's your favorite color?")
+print(f"Oh, {color_input} is a beautiful color!")
